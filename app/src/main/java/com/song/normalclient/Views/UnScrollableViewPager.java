982012ebeab333cc3ff5a9ecf5960495bff83f87
subei.x.song <subei.x.song@sonymobile.com>
2015-10-15 20:20:40 +0800

    Set viewPager unScrollable
    Signed-off-by: subei.x.song <subei.x.song@sonymobile.com>

---
+++ app/src/main/java/com/song/normalclient/Views/UnScrollableViewPager.java
@@ -0,0 +1,35 @@
+package com.song.normalclient.Views;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by songsubei on 15/10/15.
+ */
+public class UnScrollableViewPager extends ViewPager {
+    public UnScrollableViewPager(Context context) {
+        super(context);
+    }
+
+    public UnScrollableViewPager(Context context, AttributeSet attrs){
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        return false;
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        this.getParent().requestDisallowInterceptTouchEvent(false);
+        return super.dispatchTouchEvent(ev);
+    }
+}
